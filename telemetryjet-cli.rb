b12fe9a85e9ae0c8ef3765832b1b5b25c74769a8
Chris Dalke <chrisdalke@gmail.com>
2020-11-28 13:13:58 -0500

    Add formula for TelemetryJet CLI

---
+++ telemetryjet-cli.rb
@@ -0,0 +1,16 @@
+class TelemetryjetCli < Formula
+  desc "Monitor and control embedded hardware from your terminal."
+  homepage "https://www.dev.telemetryjet.com/products/cli/"
+  license "MIT"
+  version "0.0.1"
+  url "https://downloads.telemetryjet.com/builds/cli/macos/telemetryjet-cli-mac-0.0.1.zip"
+  sha256 "7ddd420ece0b445e74e1252a065035b19780974959d12a592fa03c9030d8b01d"
+
+  def install
+    bin.install "jet"
+  end
+
+  test do
+    system "#{bin}/jet", "--help"
+  end
+end
