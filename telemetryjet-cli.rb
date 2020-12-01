74271e766e7ca442535166de768595aacd04a6b8
Chris Dalke <chrisdalke@gmail.com>
2020-11-30 22:48:39 -0500

    Update telemetryjet-cli.rb

--- telemetryjet-cli.rb
+++ telemetryjet-cli.rb
@@ -2,7 +2,7 @@ class TelemetryjetCli < Formula
   desc "Monitor and control embedded hardware from your terminal."
   homepage "https://www.dev.telemetryjet.com/products/cli/"
   license "MIT"
-  version "0.0.1"
+  version "0.3.0"
   url "https://downloads.telemetryjet.com/builds/cli/macos/telemetryjet-cli-macos_x86-64_0.3.0.zip"
   sha256 "5e6f3731176ebd0d609e62b26a69e81e9f9b97dacbe78b0e27128fba59bd7e8f"
 
