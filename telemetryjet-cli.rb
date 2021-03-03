816ac42eab874e6a5d7b2a60eb0c7ba973988b56
Chris Dalke <chrisdalke@gmail.com>
2021-03-02 19:50:58 -0500

    Release version 0.2.1 of TelemetryJet CLI

--- telemetryjet-cli.rb
+++ telemetryjet-cli.rb
@@ -3,8 +3,8 @@ class TelemetryjetCli < Formula
   homepage "https://www.dev.telemetryjet.com/products/cli/"
   license "MIT"
   version "0.2.0"
-  url "https://downloads.telemetryjet.com/builds/cli/mac/telemetryjet-cli-macos_x86-64_0.2.0.zip"
-  sha256 "acfd026d4489ef40c22e949c40a5ae13c38b058bdc8b914b3ed8c2859dc88d2d"
+  url "https://downloads.telemetryjet.com/builds/cli/mac/telemetryjet-cli-macos_x86-64_0.2.1.zip"
+  sha256 "8be1ed869b09efc586cd9d197d9ecd9c9545fe50646289b005049787eeaec213"
 
   def install
     bin.install "bin/jet"
